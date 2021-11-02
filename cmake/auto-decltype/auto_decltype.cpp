7d44cc7b63c08d6b984bc4144abbef17a500a162
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-11-02 16:18:24 +0100

    range-based-for

--- cmake/auto-decltype/auto_decltype.cpp
+++ cmake/auto-decltype/auto_decltype.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <map>
 
 using namespace std;
 using namespace Catch::Matchers;
@@ -99,4 +100,72 @@ TEST_CASE("auto type deduction - case 2")
     auto& ar5 = tab; // int(&)[10]
 
     auto& ar6 = foo; // int(&)()
+}
+
+int bar(std::string s)
+{
+    return 665;
+}
+
+TEST_CASE("decltype")
+{
+    std::map<int, std::string> dict1 = { {1, "one"}, {2, "two"}, {3, "three"} };
+
+    auto dict2 = dict1;
+
+    REQUIRE(dict2.size() == 3);
+
+    decltype(dict1) dict3;
+
+    REQUIRE(dict3.size() == 0);
+
+    int tab[10];
+
+    auto& ref_tab = tab; // int(&)[10]
+
+    std::remove_reference_t<decltype(tab[0])> item; // int item;
+
+    decltype(bar("some text"s)) bar_result1;
+    decltype(bar(std::declval<std::string>())) bar_result2;
+}
+
+TEST_CASE("non-evaluated context")
+{
+    std::map<int, std::string> dict;
+
+    REQUIRE(dict.size() == 0);
+
+    REQUIRE(sizeof(decltype(dict[0])) == 28);
+
+    REQUIRE(dict.size() == 0);
+}
+
+template <typename T>
+auto full_name(const T& first, const T& last)
+{
+    if (first.empty())
+        return last; 
+
+    return first + last;
+}
+
+TEST_CASE("auto return type deduction for functions")
+{
+    std::string f = "Jan";
+    std::string l = "Kowalski";
+
+    auto fname = full_name(f, l);
+}
+
+template <typename T>
+decltype(auto) get_value(T& dict, const std::string& key) // -> float&
+{
+    return dict.at(key);
+}
+
+TEST_CASE("temp")
+{
+    std::map<std::string, float> math_vars = { {"pi", 3.14}, {"e", 2.72} };
+
+    get_value(math_vars, "pi") = 3.1415;
 }
--- cmake/range-based-for/range_based_for.cpp
+++ cmake/range-based-for/range_based_for.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
 #include <iostream>
-#       include <string>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -8,7 +8,7 @@ using namespace Catch::Matchers;
 
 TEST_CASE("range-based-for")
 {
-    std::vector vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     SECTION("iterating over container in C++98")
     {
@@ -22,5 +22,105 @@ TEST_CASE("range-based-for")
             std::cout << *it << " ";
         }
         std::cout << "\n";
+    }
+
+    SECTION("since C++11")
+    {
+        for (int item : vec)
+        {
+            std::cout << item << " ";
+        }
+        std::cout << "\n";
+
+        SECTION("is interpreted as")
+        {
+            for (auto it = vec.begin(); it != vec.end(); ++it)
+            {
+                int item = *it;
+
+                std::cout << item << " ";
+            }
+        }
+    }
+
+    SECTION("iteration over c-style array")
+    {
+        int tab[5] = {1, 2, 3, 4, 5};
+
+        for (int item : tab)
+        {
+            std::cout << item << " ";
+        }
+        std::cout << "\n";
+
+        SECTION("is interpreted as")
+        {
+            for (auto it = begin(tab); it != end(tab); ++it)
+            {
+                int item = *it;
+
+                std::cout << item << " ";
+            }
+        }
+    }
+
+    SECTION("initializer lists")
+    {
+        for (const auto& item : {"one"s, "two"s, "three"s})
+        {
+            std::cout << item << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+TEST_CASE("efficient range-based-for")
+{
+    std::vector<std::string> words = {"one", "two", "three"};
+
+    for (const auto& item : words)
+    {
+        std::cout << item << "\n";
+    }
+
+    SECTION("is interpreted as")
+    {
+        for (auto it = words.begin(); it != words.end(); ++it)
+        {
+            const auto& item = *it; // cc
+
+            std::cout << item << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+struct Point
+{
+    int x, y;
+};
+
+struct Triangle
+{
+    Point points_[3];
+
+    using iterator = Point*;
+    using const_iterator = const Point*;
+
+    iterator begin() { return points_; }
+    const_iterator begin() const { return points_; }
+    iterator end() { return points_ + 3; }
+    const_iterator end() const { return points_ + 3; }
+
+    // rest of implementation
+};
+
+TEST_CASE("range-based-for for custom type")
+{
+    Triangle t1{ { {1, 2},{2, 3}, {4,  5} } };
+
+    for(const Point& pt : t1)
+    {
+        std::cout << "(" << pt.x << ", " << pt.y << ")\n";
     }
 }
