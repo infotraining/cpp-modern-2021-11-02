d423954472913febf70fa2ac3164633101e54da5
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-11-04 15:56:40 +0100

    DataSet

--- cmake/move-semantics/move_semantics.cpp
+++ cmake/move-semantics/move_semantics.cpp
@@ -173,13 +173,174 @@ struct Gadget
     }
 };
 
+UniquePtr<Gadget> create_gadget()
+{
+    static int id_gen = 0;
+
+    auto ptr = UniquePtr<Gadget> {new Gadget(++id_gen)};
+
+    return ptr;
+}
+
+void use_and_destroy(UniquePtr<Gadget> ptr)
+{
+    ptr->use();
+}
+
 TEST_CASE("move semantics - UniquePtr")
 {
     UniquePtr<Gadget> pg1 {new Gadget {1, "ipad"}};
     pg1->use();
 
-    pg1 = UniquePtr<Gadget>{new Gadget(2, "smartwatch")}; // move assignment
+    pg1 = UniquePtr<Gadget> {new Gadget(2, "smartwatch")}; // move assignment
 
     UniquePtr<Gadget> pg2 = std::move(pg1); // move constructor
     pg2->use();
+
+    UniquePtr<Gadget> pg3 = create_gadget();
+    pg3->use();
+
+    use_and_destroy(std::move(pg3));
+
+    use_and_destroy(create_gadget());
+}
+
+class DataSet
+{
+    std::string name_;
+    int* data_;
+    size_t size_;
+
+public:
+    using iterator = int*;
+    using const_iterator = const int*;
+
+    DataSet(const std::string& name, std::initializer_list<int> list)
+        : name_ {name}
+        , size_ {list.size()}
+    {
+        data_ = new int[list.size()];
+        std::copy(list.begin(), list.end(), data_);
+    }
+
+    DataSet(const DataSet& other)
+        : name_(other.name_)
+        , size_(other.size_)
+    {
+        std::cout << "DataSet(cc)\n";
+        data_ = new int[size_];
+        std::copy(other.begin(), other.end(), data_);
+    }
+
+    DataSet& operator=(const DataSet& other)
+    {
+        if (this != &other)
+        {
+            delete[] data_;
+
+            name_ = other.name_;
+            size_ = other.size_;
+            data_ = new int[size_];
+            std::copy(other.begin(), other.end(), data_);
+
+            std::cout << "DataSet=(cc)\n";
+        }
+
+        return *this;
+    }
+
+    DataSet(DataSet&& other)
+        : name_ {std::move(other.name_)}
+        , data_ {other.data_}
+        , size_ {other.size_}
+    {
+        other.size_ = 0;
+        other.data_ = nullptr;
+
+        std::cout << "DataSet(mv)\n";
+    }
+
+    DataSet& operator=(DataSet&& other)
+    {
+        if (this != &other)
+        {
+            delete[] data_;
+
+            name_ = std::move(other.name_);
+
+            size_ = other.size_;
+            other.size_ = 0;
+
+            data_ = other.data_;
+            other.data_ = nullptr;
+
+            std::cout << "DataSet=(mv)\n";
+        }
+        return *this;
+    }
+
+    ~DataSet()
+    {
+        delete[] data_;
+    }
+
+    iterator begin()
+    {
+        return data_;
+    }
+
+    iterator end()
+    {
+        return data_ + size_;
+    }
+
+    const_iterator begin() const
+    {
+        return data_;
+    }
+
+    const_iterator end() const
+    {
+        return data_ + size_;
+    }
+};
+
+DataSet create_data_set()
+{
+    DataSet ds {"data-set-one", {54, 6, 34, 235, 64356, 235, 23}};
+
+    return ds;
+}
+
+namespace LegacyCode
+{
+    DataSet* create_data_set()
+    {
+        DataSet ds* = new DataSet {"data-set-one", {54, 6, 34, 235, 64356, 235, 23}};
+
+        return ds;
+    }
+}
+
+TEST_CASE("DataSet")
+{
+    DataSet ds1 {"ds1", {1, 2, 3, 4, 5}};
+
+    DataSet backup = ds1; // copy
+
+    for (const auto& item : backup)
+    {
+        std::cout << item << " ";
+    }
+    std::cout << "\n";
+
+    DataSet ds2 = create_data_set();
+
+    DataSet target = std::move(ds2);
+
+    for (const auto& item : target)
+    {
+        std::cout << item << " ";
+    }
+    std::cout << "\n";
 }
