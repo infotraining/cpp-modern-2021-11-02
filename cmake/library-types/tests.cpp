7e30db5a4820cce0de6bbd9db0981894b0313e60
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-11-26 16:36:11 +0100

    std::variant

--- cmake/library-types/tests.cpp
+++ cmake/library-types/tests.cpp
@@ -7,6 +7,9 @@
 #include <atomic>
 #include <charconv>
 #include <array>
+#include <any>
+#include <map>
+#include <variant>
 
 using namespace std;
 
@@ -195,4 +198,214 @@ TEST_CASE("optional & reference")
     opt_ref.value().get() = 20;
 
     REQUIRE(x == 20);
+}
+
+/////////////////////////////////////////////////////////////////
+// any
+
+class Dictionary
+{
+    using MapType = std::map<std::string, std::any>;
+    using PairType = MapType::value_type;
+public:
+    Dictionary(std::initializer_list<PairType> _items) : items(_items)
+    {
+    }
+
+    template<typename T>
+    T get(std::string key)
+    {
+        return std::any_cast<T>(items.at(key));
+    }
+
+private:
+    std::map<std::string, std::any> items;
+};
+
+TEST_CASE("any")
+{
+    std::any anything;
+
+    REQUIRE(anything.has_value() == false);
+
+    anything = 42;
+    anything = "string"s;
+    anything = 3.14;
+    anything = std::vector{1, 2, 3};
+
+    std::vector<int> vec = std::any_cast<std::vector<int>>(anything);
+    REQUIRE(vec == std::vector{1, 2, 3});
+
+    REQUIRE_THROWS_AS(std::any_cast<double>(anything), std::bad_any_cast);
+
+    anything = 665;
+
+    int* ptr_value = std::any_cast<int>(&anything);
+
+    if (ptr_value)
+        std::cout << *ptr_value << "\n";
+
+
+    SECTION("use-case with map")
+    {
+        Dictionary dict = { {"address"s, "Miodowa 33"s}, {"age", 75}, {"data", std::vector{1, 2, 3} } };
+
+        REQUIRE(dict.get<std::string>("address") == "Miodowa 33"s);
+        REQUIRE(dict.get<int>("age") == 75);
+    }
+}
+
+//////////////////////////////////////////////////////////
+// variant
+
+class PrintVisitor
+{
+public:
+    void operator()(int x) const 
+    {
+        std::cout << "int: " << x << "\n";
+    }
+
+    void operator()(const std::string& s) const
+    {
+        std::cout << "string: " << s << "\n";
+    }
+
+    void operator()(double x) const 
+    {
+        std::cout << "double: " << x << "\n";
+    }
+
+    void operator()(const std::vector<int>& v) const 
+    {
+        std::cout << "vector: ";
+        for(const auto& item : v)
+            std::cout << item << " ";
+        std::cout << "\n";
+    }
+};
+
+TEST_CASE("variant")
+{
+    std::variant<int, std::string, double, std::vector<int>> v1;
+
+    REQUIRE(std::holds_alternative<int>(v1) == true);
+
+    v1 = 3.14;
+    REQUIRE(v1.index() == 2);
+    v1 = "text"s;
+    v1 = std::vector{1, 2, 3};
+    v1 = 665;
+
+    auto& item = std::get<int>(v1);
+    REQUIRE(item == 665);    
+
+    REQUIRE_THROWS_AS(std::get<double>(v1), std::bad_variant_access);
+
+    int* ptr_value = std::get_if<int>(&v1);
+
+    if (ptr_value)
+        *ptr_value = 898;
+
+    v1 = std::vector{1, 2, 3};
+
+    std::visit(PrintVisitor{}, v1);
+}
+
+struct Square
+{
+    int size;
+};
+
+struct Rectangle
+{
+    int width, height;
+};
+
+struct Circle
+{
+    int radius;
+};
+
+struct Triangle
+{
+
+};
+
+class AreaVisitor
+{
+public:
+    double operator()(const Rectangle& rec)
+    {
+        return rec.height * rec.width;
+    }
+
+    double operator()(const Circle& circ)
+    {
+        return 3.14 * circ.radius * circ.radius;
+    }
+
+    double operator()(const Square& sq)
+    {
+        return sq.size * sq.size;
+    }
+};
+
+template <typename... TClosures>
+struct overloaded : TClosures...
+{
+    using TClosures::operator()...;
+};
+
+template <typename... TClosures>
+overloaded(TClosures...) -> overloaded<TClosures...>;
+
+TEST_CASE("polymorphism with std::variant")
+{
+    using Shape = std::variant<Rectangle, Circle, Square>;
+
+    std::vector<Shape> shapes;
+
+    shapes.push_back(Rectangle{10, 20});
+    shapes.push_back(Circle{10});
+    shapes.push_back(Square{40});
+
+    double area{};
+
+    auto area_visitor = overloaded {
+        [](const Rectangle& rec) -> double
+        {
+            return rec.height * rec.width;
+        },
+        [](const Circle& circ) -> double
+        {
+            return 3.14 * circ.radius * circ.radius;
+        },
+        [](const Square& sq) -> double
+        {
+            return sq.size * sq.size;
+        }
+    };
+
+    for(const auto& s : shapes)
+        area += std::visit(area_visitor, s);
+
+    std::cout << "Area: " << area << "\n";
+}
+
+[[nodiscard]] std::variant<std::string, std::errc> load_from_file(const std::string& filename)
+{
+    if (filename == "evil")
+        return std::errc{13};
+    return "content"s;
+}
+
+TEST_CASE("load_from_file")
+{
+    auto result = load_from_file("evil");
+
+    std::visit(overloaded{ 
+        [](const std::string& s) { std::cout << s << "\n"; },
+        [](std::errc ec) { std::cout << "Error: " << static_cast<int>(ec) << "\n";}
+    }, result);
 }
